be20c5ec5a8df300f1e485977dba81f6f88de423
wanggang_test <wanggang_test@guanghe.tv>
2020-06-29 14:07:02 +0800

    commit

--- allure_run.sh
+++ allure_run.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-pip list
 allure serve alluredir_reports -p 3000
--- har2case_all.sh
+++ har2case_all.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-ls -l
 for i in $(ls -l har/ | grep .har | awk {'print $9'}); do
   har2case har/$i
 done
